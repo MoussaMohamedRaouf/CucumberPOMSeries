0381e42b69086dd61318614793be65158a5ee488
Moussa Mohamed Raouf <moussaraouf@outlook.fr>
2022-10-03 17:12:10 +0200

    Update pom file

--- src/test/java/testrunners/MyTestCase.java
+++ src/test/java/testrunners/MyTestCase.java
@@ -6,12 +6,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features ={"src/test/resources/AppFeatures"},
+        features ={"src/test/resources/AppFeatures/contactUs.feature"},
         glue = {"stepsdefinition","AppHooks"},
         plugin = {
                 "pretty",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "timeline:test-output-thread/"
+                //"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                //"timeline:test-output-thread/",
+                "html:target/cucumber",
+                "json:target/cucumber.json"
         }
 )
 public class MyTestCase {
