5b9b05212a38c5bcd25608f0680b6e409b841e3a
Moussa Mohamed Raouf <moussaraouf@outlook.fr>
2022-10-03 17:14:43 +0200

    Update pom file

--- src/test/java/testrunners/MyTestCase.java
+++ src/test/java/testrunners/MyTestCase.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features ={"src/test/resources/AppFeatures/contactUs.feature"},
+        features ={"src/test/resources/AppFeatures"},
         glue = {"stepsdefinition","AppHooks"},
         plugin = {
                 "pretty",
